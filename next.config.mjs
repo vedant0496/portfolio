118a8d7ecec61705af3e1cd4955b6ef979f9cc84
Vedant Kaushik <vekaushi@blueplanet.com>
2026-03-30 15:00:16 +0530

    chore: setup GitHub Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,14 @@
 /** @type {import('next').NextConfig} */
+const isGithubActions = process.env.GITHUB_ACTIONS === 'true';
+const repoName = 'portfolio';
+
 const nextConfig = {
+  output: 'export',
+  trailingSlash: true,
+  basePath: isGithubActions ? `/${repoName}` : '',
+  assetPrefix: isGithubActions ? `/${repoName}/` : '',
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         protocol: 'https',
